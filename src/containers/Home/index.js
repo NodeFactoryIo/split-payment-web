e03ca32d020893fc53b7c086f30e28608876a9e7
Belma Gutlic <belma.dzi@gmail.com>
2019-08-24 00:48:12 +0200

    Add web3 provider

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Hero } from '../../components/Hero';
+import { Web3Provider } from '../../Web3Provider';
+import { TransactionsList } from '../TransactionsList';
 
 export class Home extends React.Component {
   render() {
@@ -7,8 +9,11 @@ export class Home extends React.Component {
       <div>
         <Hero />
 
+        <Web3Provider>
+          <TransactionsList/>
+        </Web3Provider>
 
-
+        <footer>Powered by Etherscan.io APIs</footer>
       </div>
     )
   }
--- src/containers/TransactionsList/index.js
+++ src/containers/TransactionsList/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import { Web3Context } from '../../Web3Provider';
 
 export class TransactionsList extends React.Component {
+  static contextType = Web3Context;
+
+  componentDidMount() {
+    const { provider } = this.context;
+
+    provider.listAccounts().then((accounts) => {
+      console.log(accounts);
+    });
+  }
+
   render() {
     return (
       <div>
